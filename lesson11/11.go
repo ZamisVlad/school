2bc27e6ed65cfd911407664690543dae5086a217
Vladislavs Zamišļajevs <vzamislajevs@edu.riga.lv>
2021-10-14 10:28:03 +0300

    added the lesson task

--- lesson11/11.go
+++ lesson11/11.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println("The program finds the sum of digits of N.")
+	n := 321
+	for ; n != 0; n /= 10 {
+		fmt.Println(n)
+		s := n % 10
+		fmt.Println(s)
+
+	}
 
 }
